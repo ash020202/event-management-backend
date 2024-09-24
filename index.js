8731f41894bf73191728f8c1623d33331a83f800
ash020202 <130071750+ash020202@users.noreply.github.com>
2024-09-24 14:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
   res.status(404).json({ error: 'Invalid endpoint' });
 });
 
+app.get('/health', (req, res) => {
+    res.send('Server is up and running!');
+});
+
 var port = process.env.PORT || 8080;
 
 app.listen(port, async () => {
